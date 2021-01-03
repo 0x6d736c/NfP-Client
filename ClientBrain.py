ebb8f752055ced834618d7fe83b597a9f64c70a0
Michael Liebegott <liebem@seas.upenn.edu>
2021-01-03 15:10:56 -0500

    Add torrent decoder method

--- ClientBrain.py
+++ ClientBrain.py
@@ -1,4 +1,6 @@
 from random import randint
+import bencode
+
 version_number = "0001" #Current NfP Client version number. Updated 3 January, 2021
 
 class ClientBrain:
@@ -18,3 +20,20 @@ class ClientBrain:
         number_suffix = "".join([str(randint(0, 9)) for number in range(12)])   #Generate string of numbers to append to ID
         
         return f"-NF{version_number}-{number_suffix}"   #Returns a string with a randomly generated ID.
+
+    @staticmethod
+    def decode_torrent(file_path):
+        """Extracts the torrent information from a .torrent file.
+        
+        Returns:
+            An ordered dictionary of decoded bencoding of the inputted .torrent file."""
+
+        #Open specified file in binary read mode. Read-in data from file. Decode it using bencode library.
+        with open(file_path, "rb") as torrent_file:
+            torrent_data = torrent_file.read()
+            decoded_data = bencode.decode(torrent_data)
+
+        return decoded_data
+
+
+ClientBrain.decode_torrent("ubuntu-20.10-desktop-amd64.iso.torrent")
